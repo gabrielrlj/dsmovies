c1f72e0349cb076f7b5928f2576f8cd8a9841990
Gabriel Jardim <gabrielrlj99@outlook.com>
2022-01-13 22:41:59 -0300

    domain e seed

---
+++ backend/src/main/java/com/jardim/dsmovie/model/Score.java
@@ -0,0 +1,45 @@
+package com.jardim.dsmovie.model;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_score")
+public class Score {
+
+    @EmbeddedId
+    private MovieUser id = new MovieUser();
+    private Double value;
+
+    public Score() {}
+
+    public void setMovie(Movie movie){
+        this.id.setMovie(movie);
+    }
+
+    public void setUser(User user){
+        this.id.setUser(user);
+    }
+
+    public Score(MovieUser id, Double value) {
+        this.id = id;
+        this.value = value;
+    }
+
+    public MovieUser getId() {
+        return id;
+    }
+
+    public void setId(MovieUser id) {
+        this.id = id;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+}
