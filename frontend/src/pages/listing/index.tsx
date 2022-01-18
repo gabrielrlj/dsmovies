fab97afa64d8a44ae9de51e5f5c441259721194f
Gabriel Jardim <gabrielrlj99@outlook.com>
2022-01-17 21:33:41 -0300

    react hooks

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,17 +1,28 @@
 import axios from "axios";
 import MovieCard from "components/moviecard";
 import Pagination from "components/pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing(){
 
-    axios.get(`${BASE_URL}`)
+    const [pageNumber, setPageNumber] = useState(0);
+    
+    useEffect(() => {
+
+        axios.get(`${BASE_URL}/movies?page=2`)
         .then( response => {
-            console.log(response.data);
+            const data = response.data as MoviePage;
+            setPageNumber(data.number); 
         });
+
+    }, []);
+
 
     return (
         <>
+            <p>{pageNumber}</p>
             <Pagination/>
             <div className="container">
                 <div className="row">
