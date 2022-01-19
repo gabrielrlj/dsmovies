10bd22509e774ae6a8ac29aaa9566aaa9beeb18b
Gabriel Jardim <gabrielrlj99@outlook.com>
2022-01-18 21:39:34 -0300

    implementando integracao

--- frontend/src/components/stars/index.tsx
+++ frontend/src/components/stars/index.tsx
@@ -3,15 +3,58 @@ import {ReactComponent as StarHalf} from "assets/img/star-half.svg";
 import {ReactComponent as StarEmpty} from "assets/img/star-empty.svg";
 import "./styles.css"
 
-function Stars() {
+type Props = {
+    score: number;
+}
+type StarProps = {
+    fill: number;
+}
+
+function Star({fill} : StarProps){
+    if (fill === 0){
+        return <StarEmpty/>
+
+    } else if (fill === 0.5){
+        return <StarHalf/>
+
+    } else if (fill === 1){
+        return <StarFull/>
+
+    }
+
+    return <StarEmpty/>
+}
+
+
+function getFills(score: number) {
+
+    const fills = [0, 0, 0, 0, 0];
+  
+    const integerPart = Math.floor(score);
+  
+    for (let i = 0; i < integerPart; i++) {
+      fills[i] = 1;
+    }
+  
+    const diff = score - integerPart;
+    if (diff > 0) {
+      fills[integerPart] = 0.5;
+    }
+  
+    return fills;
+}
+
+function Stars({score} : Props) {
+
+    const fills = getFills(score);
 
     return (
         <div className="dsmovie-stars-container">
-            <StarFull />
-            <StarFull />
-            <StarFull />
-            <StarHalf />
-            <StarEmpty />
+            {fills.map(fill => {
+                return(
+                    <Star fill={fill} />
+                );
+            })}
         </div>
     );
 }
